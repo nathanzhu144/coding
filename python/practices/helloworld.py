a98564da799120c4fbec978b92c29622d80a308f
nathanzhu144 <znathan@umich.edu>
2018-02-11 14:54:17 -0500

    znathan/changed helloworld.py to add more texts

--- python/practices/helloworld.py
+++ python/practices/helloworld.py
@@ -1,3 +1,25 @@
 #Learning hello world. No iostream.
 
-print("Hello world!")
+my_name = 'Nathan'
+
+print('Hello world!\n')
+print('My name is', my_name, 'Nathan, and I like to eat apples.')
+print('I also love proofs by induction!')
+
+person_name = input('Hi, what\'s your name?\n')
+
+
+if person_name == "":
+    print('I\'m sorry that your name so boring\n')
+elif person_name[0] >= 'F' or person_name[0] >= 'F':
+    print('I like that name.  In the future, I will use that name.')
+    my_name = person_name
+    print('Nice to meet you',person_name,'!\n\n')
+    print('I like that name.')
+    print('My name is', person_name, ', and I like to eat apples.')
+    print('Thanks for your name!')
+elif person_name.length:
+    print('Cool story bro!\n')
+    
+
+
