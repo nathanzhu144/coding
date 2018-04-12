ac6312f5d50f606f7440746a84500467bd90597b
nathanzhu144 <znathan@umich.edu>
2018-04-12 16:55:31 -0400

    mergesort working

--- sorts/mergesort.cpp
+++ sorts/mergesort.cpp
@@ -6,17 +6,17 @@
 
 #include <iostream>
 
-void merge(int arr[], int start, int mid, int end){
+void merge(int arr[], int start, int mid, const int end){
     //cmp1 and cmp2 tracks the beginning of the unmerged sections of the two subarrays
     int cmp1 = start;
     int cmp2 = mid + 1;
 
     //declares a temp array to hold our sorted elements, temp_arr_index tracks the first unfilled position in the array
-    int temp_arr[start - end + 1]; 
+    int temp_arr[end - start + 1]; 
     int temp_arr_index = 0;
 
     //merges two sorted subarrays into temp_arr
-    for(int i = 0; i <= start; ++i){
+    for(int i = start; i <= end; ++i){
         if(cmp1 > mid)                                     //all of first subarray is merged, so second is not finished merging into temp_arr
             temp_arr[temp_arr_index++] = arr[cmp2++];
         else if(cmp2 > end)                                //all of second subarray is merged, so first is not finished merging into temp_arr
@@ -52,9 +52,9 @@ void print_arr(int arr[], int size){
 
 
 int main(){
-    int arr1[7] = {-3, -2, 3, 43, 8, 23, 1};
-    merge_sort(arr1, 0, 7);
-    print_arr(arr1, 7);
+    int arr1[7] = {15, 7, 3, 43, 8, 23, 1};
+    merge_sort(arr1, 0, 6);
+    print_arr(arr1, 6);
 
     return 0;
 }
