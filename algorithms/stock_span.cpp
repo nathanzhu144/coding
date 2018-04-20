e782fba6c0915e874b061f269808932562123157
nathanzhu144 <znathan@umich.edu>
2018-04-20 08:26:13 -0400

    finished stockspan.cpp

--- algorithms/stock_span.cpp
+++ algorithms/stock_span.cpp
@@ -1,70 +1,106 @@
-// a*- linear time solution for stock span problem
+/**
+*  znathan 4/20/2018
+*  
+*  This is an implementation of an efficient way to solve the stock-span problem
+*  in O(n) time instead of O(n^2) time with the use of a stack.  
+*
+*  The stack keeps track of the list of highest stock prices relevant to each current
+*  stock span calculation.
+*  
+*  Ex. Days      0    1    2    3    4    5 
+*      Prices   100   80   60   70  71  102
+*
+*
+*      On day 2 the stack will look like this:
+*
+*                   |  2  | 
+*                   |  1  |
+*                   |  0  |
+*
+*      The array of stock spans should look like this {1, 1, 1, ?, ?, ?}
+*
+*
+*      On day 3 the stack will look like this:
+*
+*                   |  3  | 
+*                   |  1  |
+*                   |  0  |
+*
+*      The array of stock spans should look like this {1, 1, 1, 2, ?, ?}
+*
+*
+*      On day 4 the stack will look like this:
+*
+*                   |  4  | 
+*                   |  1  |
+*                   |  0  |
+*
+*      The array of stock spans should look like this {1, 1, 1, 2, 3, ?}
+*    
+*
+*      On day 5 the stack will look like this:
+*
+*                   |  5  | 
+*
+*      The array of stock spans should look like this {1, 1, 1, 2, 3, 6}
+*/
 #include <iostream>
 #include <stack>
-#include <string>
 
-using namespace std;
  
-// A stack based efficient method to calculate stock span values
-void calculateSpan(int price[], int n, int S[])
+
+void calculate_span(int price[], int n, int S[])
 {
-   // Create a stack and push index of first element to it
-   stack<int> st;
-   st.push(0);
- 
-   // Span value of first element is always 1
-   S[0] = 1;
- 
-   // Calculate span values for rest of the elements
-   for (int i = 1; i < n; i++)
-   {
-      // Pop elements from stack while stack is not empty and top of
-      // stack is smaller than price[i]
-      while (!st.empty() && price[st.top()] <= price[i])
-         st.pop();
- 
-      // If stack becomes empty, then price[i] is greater than all elements
-      // on left of it, i.e., price[0], price[1],..price[i-1].  Else price[i]
-      // is greater than elements after top of stack
-      S[i] = (st.empty())? (i + 1) : (i - st.top());
- 
-      // Push this element to stack
-      st.push(i);
-   }
+    S[0] = 1;                                  //first day always has a span of 1
+
+    std::stack<int> st;  
+    st.push(0);                                //st is an index of days, keeps track of largest elements
+
+    for(int i = 1; i < n; ++i){
+        while(!st.empty() && price[st.top()] <= price[i])
+            st.pop();
+
+        if(st.empty())
+            S[i] = i + 1;
+        else
+            S[i] = i - st.top();
+
+        st.push(i);
+    }
 }
  
 // A utility function to print elements of array
-void printArray(int arr[], int n)
+void print_array(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
-      cout << arr[i] << " ";
+      std::cout << arr[i] << " ";
 }
-
-
  
 // Driver program to test above function
 int main()
 {
-
     int price[] = {10, 4, 5, 90, 120, 80};
     int n = sizeof(price)/sizeof(price[0]);
     int S[n];
  
     // Fill the span values in array S[]
-    calculateSpan(price, n, S);
+    calculate_span(price, n, S);
  
     // print the calculated span values
-    printArray(S, n);
-
-    int a;
-    a+=2;
-    std::cout << a;
-
-    for(int i = 0; i < 10; ++i){
-        int g = 3;
-        int* dog = new int[g - i];
-    }
+    std::cout << "stock span of array {10, 4, 5, 90, 120, 80} is ";
+    print_array(S, n);
+    std::cout << std::endl;
 
 
+    int price2[] = {100, 80, 60, 70, 71, 102};
+    int n2 = sizeof(price2)/sizeof(price2[0]);
+    int S2[n2];
+    calculate_span(price2, n2, S2);
+
+    std::cout << "stock span of array {100, 80, 60, 70, 71, 102} is ";
+    print_array(S2, n2);
+    std::cout << std::endl;
+
+ 
     return 0;
 }
