afbc6d0ce032b68ce5745d9625df3d25e89c1dc1
nathanzhu144 <znathan@umich.edu>
2018-10-07 00:58:24 -0400

    added zigzag

---
+++ coding_exercises/array/zigzag.py
@@ -0,0 +1,19 @@
+#  12:50 am 10/7/2018 Foundry Lofts
+#  Nathan Zhu
+#  https://www.geeksforgeeks.org/convert-array-into-zig-zag-fashion/
+#
+#  How to change a number list to a zigzag
+def zigzag(arr):
+    for i in range(0, len(arr) - 1):
+        if i % 2 is 0:
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        else:
+            if arr[i] < arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+    print(arr)
+
+if __name__ == "__main__":
+    arr = [4, 3, 7, 8, 6, 2, 1]
+    zigzag(arr)
