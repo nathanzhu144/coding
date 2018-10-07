1212a84e84f76c698d2b105e1fd780c9bb861187
nathanzhu144 <znathan@umich.edu>
2018-10-06 20:57:19 -0400

    added train station and leaders exercises to array part

---
+++ coding_exercises/array/leaders.py
@@ -0,0 +1,17 @@
+
+
+def find_leaders(arr):
+    numleaders = 0
+    leader_list = [arr[-1]]
+    max = arr[-1]
+
+    for i in xrange(len(arr) - 1, 0, -1):
+        if arr[i] > max:
+            max = arr[i]
+            leader_list.append(arr[i])
+
+    return leader_list
+
+if __name__ == "__main__":
+    arr = [16,17,4,3, 5, 2]
+    print(find_leaders(arr))
---
+++ coding_exercises/array/train_station.py
@@ -0,0 +1,41 @@
+def min_trains(arrivals, departures):
+    arrivals.sort()
+    departures.sort()
+
+    #This first arrival must be before the first departure
+    i = 1
+    j = 0
+    
+    #We need at least 1 platform for first train
+    num_platforms = 1
+    max_platforms = 1
+
+    # merge-sort like iteration thru both lists
+    while i < len(arrivals) and j < len(departures):
+        #next arrival before next depart
+        if arrivals[i] < departures[j]:
+            i += 1
+            num_platforms += 1
+        #next arrival after next depart
+        elif arrivals[i] > departures[j]:
+            j += 1
+            num_platforms -= 1
+        #next arrival same next depart
+        elif arrivals[i] is departures[j]:
+            i += 1
+            j += 1
+        
+        #check see if new platform count greater
+        if num_platforms > max_platforms:
+            max_platforms = num_platforms
+        
+    return max_platforms
+        
+
+
+if __name__ == "__main__":
+    arrivals = [900,  940, 950,  1100, 1500, 1800]
+    departures = [910, 1200, 1120, 1130, 1900, 2000]
+
+    
+    print(min_trains(arrivals, departures))
