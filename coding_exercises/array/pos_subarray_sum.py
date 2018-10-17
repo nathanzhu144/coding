609a585429a43f8fee1eaf08735641b6701daed7
nathanzhu144 <znathan@umich.edu>
2018-10-17 08:15:55 -0400

    Added pos subarray problem, sliding window

--- coding_exercises/array/pos_subarray_sum.py
+++ coding_exercises/array/pos_subarray_sum.py
@@ -7,26 +7,44 @@
 #  question.
 # 
 #  http://blog.gainlo.co/index.php/2016/06/01/subarray-with-given-sum/
+#  http://blog.gainlo.co/index.php/2017/03/03/chapter-3-practice-coding-questions-complete-guide-google-interview-preparation/
+#  http://blog.gainlo.co/index.php/2016/04/08/if-a-string-contains-an-anagram-of-another-string/
+#  https://www.geeksforgeeks.org/find-subarray-with-given-sum/
 
 def pos_subarray_sum(arr, sum):
     start_index = 0
-    sum_so_far = 0
+    sum_so_far = arr[0]
 
-    for i in range(len(arr)):
-        sum_so_far += arr[i]
+    # array has to iterate one past, 
+    # in edge cases where sum can only be made
+    # with something inclusive of last item in arr
+    for i in range(1, len(arr) + 1):
+
+        while sum_so_far > sum:
+            sum_so_far -= arr[start_index]
+            start_index += 1
 
         if sum_so_far == sum:
             return i - start_index
 
-        if sum_so_far > sum:
-            sum_so_far -= arr[start_index]
-            start_index += 1
+        #only increment if in bounds
+        if i < len(arr):
+            sum_so_far += arr[i]
+
+        
 
 
 if __name__ == "__main__":
-    #should be 3
+    #should be 4
     print(pos_subarray_sum([1, 2, 4, 7, 3, 13, 12, 1, 2, 3], 14))
     #should be 4
     print(pos_subarray_sum([1, 2, 4, 7, 3, 13, 12, 1, 2, 3], 16))
     #should be 2
     print(pos_subarray_sum([1, 2, 4, 7, 3, 13, 12, 1, 2, 3], 5))
+    #should be 2
+    print(pos_subarray_sum([17, 2, 4, 7, 3, 13, 12, 1, 2, 3], 10))
+    #should be 1
+    print(pos_subarray_sum([17, 2, 4, 7, 19, 13, 12, 1, 6, 3], 3))
+    #should be 1
+    print(pos_subarray_sum([6, 3], 3))
+
