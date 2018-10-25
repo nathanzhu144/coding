fd20acc432d144d04ef05ee6a5eda43e2f40552e
nathanzhu144 <znathan@umich.edu>
2018-10-24 21:57:24 -0400

    Added backtracking parenths permutator

---
+++ coding_exercises/backtrack/parenth.py
@@ -0,0 +1,26 @@
+#  Nathan Zhu
+#  October 24th, 2018
+#  Generating all pairs of valid parenthesis.  
+# 
+#  Easier than it seems with recursive backtracking.
+
+def parens_helper(all_perms, string, left, right, n):
+    if len(string) == 2 * n:
+        all_perms.append(string)
+
+    if left < n:
+        parens_helper(all_perms, string + "(", left + 1, right, n)
+
+    if right < left:
+        parens_helper(all_perms, string +  ")", left, right + 1, n)
+
+    return all_perms
+
+def genparens(n):
+    return parens_helper([], "", 0, 0, n)
+
+
+if __name__ == "__main__":
+    perms = genparens(13)
+    for i in perms:
+        print(i)
